0044ba789804370959e099c9d67079a1e773f440
maelgllt <mael.guilloteau@outlook.fr>
2025-01-12 17:15:27 +0100

    ajout dump.sql + maj readme

--- README.md
+++ README.md
@@ -7,19 +7,65 @@
 - BDD : MySQL
 - Swagger : pour la documentation
 
-## Architecture
+## Prérequis
+- Node.js (https://nodejs.org/fr)
+- MySQL (https://dev.mysql.com/downloads/)
 
-- /register: routes pour l'inscription
-- /login: routes pour la connexion
-- /tools/email-check: Vérification de l'existence d'une adresse mail.
-- /tools/spammer: Envoi de spamms.
-- /tools/password-check: Vérifier si un mot de passe fait partie des plus courants.
-- /tools/domain-info: Récupérer les sous-domaines d'un NDD.
-- /tools/ddos : simule une attaque ddos
-- /tools/random-image : changement d'image random
-- /tools/generate-password: Générateur de mot de passe sécurisé.
-- /tools/generate-identity: Générer des identités fictives.
-- /admin: Pour la gestion des logs et les droits.
+## Cloner le projet
 
-## Infos
-### Installation de l'Api
+    ```bash
+    git clone https://github.com/maelgllt/M1-MDS-2425-API.git
+    ```
+
+## Installer les dépendances
+Une fois dans le dossier du projet, installer les dépendances Node.js en utilisant la commande suivante :
+
+    ```bash
+    npm install
+    ```
+
+## Importer la base de données
+1. Avoir installé MySQL installé
+    - Ajoutez le chemin de mysql à la variable d'environnement PATH si nécessaire. Souvent le chemin est celui-ci : "C:\Program Files\MySQL\MySQL Server X.X\bin\mysql.exe"
+
+2. Ouvrir un terminal ou une invite de commande (cmd).
+
+3. Se connecter au serveur MySQL avec la commande :
+
+   ```bash
+   mysql -u [username] -p
+   ```
+
+Par défaut, le username est : root
+
+4. Créez une base de données vide nommée **hackr_api** :
+
+    ```sql
+    CREATE DATABASE hackr_api;
+    ```
+
+Vérifier que la base est créee : 
+
+    ```sql
+    SHOW DATABASES;
+    ```
+
+5. Importer le fichier SQL
+- Quitter MySQL
+
+    ```bash
+    exit
+    ```
+
+- Exécutez cette commande pour importer le fichier dump.sql dans la base de données hackr_api :
+
+    ```bash
+    mysql -u [username] -p hackr_api < chemin/vers/dump.sql
+    ```
+
+## Démarrer le serveur
+Dans un terminal, entrer la commande :
+
+    ```bash
+    node app.js
+    ```
---
+++ dump.sql
@@ -0,0 +1,162 @@
+-- MySQL dump 10.13  Distrib 8.0.39, for Win64 (x86_64)
+--
+-- Host: localhost    Database: hackr_api
+-- ------------------------------------------------------
+-- Server version	8.0.39
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `permissions`
+--
+
+DROP TABLE IF EXISTS `permissions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `permissions` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `permissions`
+--
+
+LOCK TABLES `permissions` WRITE;
+/*!40000 ALTER TABLE `permissions` DISABLE KEYS */;
+INSERT INTO `permissions` VALUES (1,'email-check',NULL,NULL),(2,'spammer',NULL,NULL),(3,'phishing',NULL,NULL),(4,'password-check',NULL,NULL),(5,'domain-info',NULL,NULL),(6,'ddos',NULL,NULL),(7,'random_image',NULL,NULL),(8,'generate-identity',NULL,NULL),(9,'crawler',NULL,NULL),(10,'generate-password',NULL,NULL);
+/*!40000 ALTER TABLE `permissions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role_permissions`
+--
+
+DROP TABLE IF EXISTS `role_permissions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `role_permissions` (
+  `roleId` int NOT NULL,
+  `permissionId` int NOT NULL,
+  KEY `roleId` (`roleId`),
+  KEY `permissionId` (`permissionId`),
+  CONSTRAINT `role_permissions_ibfk_1` FOREIGN KEY (`roleId`) REFERENCES `roles` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `role_permissions_ibfk_2` FOREIGN KEY (`permissionId`) REFERENCES `permissions` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role_permissions`
+--
+
+LOCK TABLES `role_permissions` WRITE;
+/*!40000 ALTER TABLE `role_permissions` DISABLE KEYS */;
+INSERT INTO `role_permissions` VALUES (2,1),(1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(1,7),(1,8),(1,9),(1,10);
+/*!40000 ALTER TABLE `role_permissions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `createdAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'admin','2024-10-11 09:45:32','2024-10-11 09:45:32'),(2,'user','2024-10-11 09:45:32','2024-10-11 09:45:32');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sequelizemeta`
+--
+
+DROP TABLE IF EXISTS `sequelizemeta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sequelizemeta` (
+  `name` varchar(255) COLLATE utf8mb3_unicode_ci NOT NULL,
+  PRIMARY KEY (`name`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sequelizemeta`
+--
+
+LOCK TABLES `sequelizemeta` WRITE;
+/*!40000 ALTER TABLE `sequelizemeta` DISABLE KEYS */;
+INSERT INTO `sequelizemeta` VALUES ('20241010131748-create-user.js'),('20241011091317-create-role-table.js'),('20241011092951-add-roleId-to-users.js'),('20241121105121-create-permissions-table.js'),('20241121133959-create-role-permissions.js');
+/*!40000 ALTER TABLE `sequelizemeta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `createdAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `roleId` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  KEY `fk_role_id` (`roleId`),
+  CONSTRAINT `fk_role_id` FOREIGN KEY (`roleId`) REFERENCES `roles` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'mael.guilloteau@my-digital-school.org','password','2024-10-10 14:13:05','2024-10-10 14:13:05',1),(3,'mael.guilloteau@outlook.fr','password','2024-10-11 09:34:49','2024-10-11 09:34:49',1),(4,'enzo.lavaud@my-digital-school.org','123','2024-10-11 09:53:20','2024-10-11 09:53:20',2),(5,'admin@gmail.fr','admin','2024-10-11 11:37:19','2024-10-11 11:37:19',1),(10,'user@gmail.fr','user','2024-10-11 11:54:37','2024-10-11 11:54:37',2);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-01-12 16:12:32
